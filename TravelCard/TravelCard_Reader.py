171b1f7773919238dbdddc353e6894829a5ea92d
SpenserCai <spensercai@gmail.com>
2017-08-02 23:09:30 +0800

    更新交通卡信息的余额查询，添加有效期查询

--- TravelCard/TravelCard_Reader.py
+++ TravelCard/TravelCard_Reader.py
@@ -10,6 +10,13 @@ def getBalance(tvlStr):
     else:
         return ''
 
+def getEffdata(tvlStr):
+    if len(tvlStr) >= 70:
+        effdata = tvlStr[42:46] + '-' + tvlStr[46:48] + '-' + tvlStr[48:50] + ' ~ ' + tvlStr[50:54] + '-' + tvlStr[54:56] + '-' + tvlStr[56:58]
+        return str(effdata)
+    else:
+        return ""
+
 os.system('clear')
 # 打开proxmark3
 pm3 = subprocess.Popen("/home/satan/Program/proxmark3-iceman/client/proxmark3 /dev/ttyACM0", shell=True, stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE)
@@ -17,16 +24,18 @@ pm3 = subprocess.Popen("/home/satan/Program/proxmark3-iceman/client/proxmark3 /d
 pm3.stdin.write('hf 14a raw -c -s 0200A40000023F01 -p\n')
 # 读取余额
 pm3.stdin.write('hf 14a raw -c 03805c000204 -p\n')
+# 读取卡片信息
+pm3.stdin.write('hf 14a raw -c 0200B0950000 -p\n')
 # 关闭卡片
 pm3.stdin.write('hf 14a raw -a\n')
 out = pm3.communicate()[0]
 outarray = out.split("pm3 --> ")
 balance = getBalance(outarray[2].split('\n')[2].replace(" ",""))
-
+effdata = getEffdata(outarray[3].split('\n')[2].replace(" ",""))
 print '----------------------------------------------------------------------------------------'
 print '                                     交通卡信息显示                                     '
 print '----------------------------------------------------------------------------------------'
 print '卡内余额：' + balance
-
+print '有效日期：' + effdata
 os.system('rm .history')
 os.system('rm proxmark3.log')
